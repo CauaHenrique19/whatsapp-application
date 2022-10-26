8aa7b63d393c71cb5529fa18db66aad6a1abbdb1
CauaHenrique19 <cauah6002@gmail.com>
2022-10-25 22:48:07 -0300

    Fixing provider of multiton

--- src/infra/multiton/providers/multiton.provider.ts
+++ src/infra/multiton/providers/multiton.provider.ts
@@ -1,8 +1,8 @@
+import { WhatsappClientInterface } from 'src/data/protocols/whatsapp';
 import { Provider } from '@nestjs/common';
 import { Multiton } from '../implementations';
-import { MULTITON_LOCAL } from './providers';
 
-export const produtoProvider: Provider = {
-  provide: MULTITON_LOCAL,
-  useValue: Multiton,
+export const MultitonProvider: Provider = {
+  provide: Multiton<WhatsappClientInterface>,
+  useClass: Multiton<WhatsappClientInterface>,
 };
