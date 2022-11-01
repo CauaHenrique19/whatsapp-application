160bcadc8347b95ecc6cc197d2ac3b4da5d9447b
CauaHenrique19 <cauah6002@gmail.com>
2022-11-01 00:16:35 -0300

    Adapting adapter to new data to save in token

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,22 +1,22 @@
-import { Decrypter, DecrypterResult, Encrypter } from 'src/data/protocols/cryptography';
+import { Decrypter, Encrypter, InfoToken } from 'src/data/protocols/cryptography';
 import { sign, verify } from 'jsonwebtoken';
 
 export class JwtAdapter implements Encrypter, Decrypter {
   constructor(private readonly secret: string) {}
 
-  async encrypt(value: string): Promise<string> {
+  async encrypt(value: InfoToken): Promise<string> {
     const accessToken = await sign({ id: value }, this.secret);
     return accessToken;
   }
 
-  async decrypt(token: string): Promise<DecrypterResult> {
-    const decoded = new Promise<DecrypterResult>(async (resolve) => {
-      await verify(token, this.secret, async (error, encoded: DecrypterResult) => {
+  async decrypt(token: string): Promise<InfoToken> {
+    const decoded = new Promise<InfoToken>(async (resolve) => {
+      await verify(token, this.secret, async (error, encoded: { id: InfoToken }) => {
         if (error) {
           resolve(null);
         }
 
-        resolve(encoded);
+        resolve(encoded.id);
       });
     });
 
