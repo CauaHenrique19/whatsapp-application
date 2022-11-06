ae5b10752f88a4b52a42258989a774b45576d110
CauaHenrique19 <cauah6002@gmail.com>
2022-11-06 01:35:00 -0300

    Creating factory to build controller of create channel

---
+++ src/main/factories/controllers/channel/build-create-channel-controller.factory.ts
@@ -0,0 +1,23 @@
+import { Controller } from './../../../../presentation/protocols/controller';
+import { Inject } from '@nestjs/common';
+import { CreateChannelUseCase, LoadUserByEmailUseCase } from 'src/domain/usecases';
+import { CREATE_CHANNEL_FACTORY, LOAD_USER_BY_EMAIL_FACTORY } from '../../providers';
+import { AdminProxy, AuthenticationProxy } from 'src/main/proxies';
+import { CreateChannelController } from 'src/presentation/controllers/channel';
+import { JwtAdapter } from 'src/infra/cryptography/jwt-adapter';
+
+export class BuildCreateChannelControllerFactory {
+  constructor(
+    @Inject(CREATE_CHANNEL_FACTORY) private readonly createChannelUseCase: CreateChannelUseCase,
+    @Inject(LOAD_USER_BY_EMAIL_FACTORY) private readonly loadUserByEmail: LoadUserByEmailUseCase,
+  ) {}
+
+  public build(): Controller {
+    const controller = new CreateChannelController(this.createChannelUseCase);
+    const jwtAdapter = new JwtAdapter(process.env.SECRET);
+    const authenticationProxy = new AuthenticationProxy(controller, jwtAdapter);
+    const adminProxy = new AdminProxy(authenticationProxy, this.loadUserByEmail, jwtAdapter);
+
+    return adminProxy;
+  }
+}
