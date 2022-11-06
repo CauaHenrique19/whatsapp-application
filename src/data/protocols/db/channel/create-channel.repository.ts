3d7e8a4caa7266ec94677aa6d625ec2ecd0c9249
CauaHenrique19 <cauah6002@gmail.com>
2022-11-06 01:31:19 -0300

    Creating usecase of repository to create a channel

---
+++ src/data/protocols/db/channel/create-channel.repository.ts
@@ -0,0 +1,11 @@
+import { ChannelModel } from 'src/domain/models';
+import { CreateChannelUseCase } from 'src/domain/usecases';
+
+export interface CreateChannelRepository {
+  create(parameters: CreateChannelRepository.Parameters): Promise<CreateChannelRepository.Result>;
+}
+
+export namespace CreateChannelRepository {
+  export type Parameters = Omit<ChannelModel, 'client' | 'users'>;
+  export type Result = CreateChannelUseCase.Result;
+}
