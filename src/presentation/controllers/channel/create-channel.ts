9d9a5e4a69c93b7e41fa499f96a9d347c46bd611
CauaHenrique19 <cauah6002@gmail.com>
2022-11-06 01:33:46 -0300

    Creating a controller to call usecases and make validations

---
+++ src/presentation/controllers/channel/create-channel.ts
@@ -0,0 +1,31 @@
+import { CreateChannelUseCase } from 'src/domain/usecases';
+import { MissingParamError } from 'src/presentation/errors';
+import { badRequest, ok, serverError } from 'src/presentation/helpers';
+import { Controller, HttpResponse } from 'src/presentation/protocols';
+
+export class CreateChannelController implements Controller {
+  constructor(private readonly createChannelUseCase: CreateChannelUseCase) {}
+
+  async handle(data: CreateChannelController.Parameters): Promise<HttpResponse> {
+    try {
+      const mandatoryFields: CreateChannelController.MandatoryFields[] = ['clientId', 'name'];
+
+      for (const field of mandatoryFields) {
+        if (!data[field] === null) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+
+      const channel = await this.createChannelUseCase.create({ clientId: data.clientId, name: data.name });
+
+      return ok(channel);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
+
+export namespace CreateChannelController {
+  export type Parameters = CreateChannelUseCase.Parameters;
+  export type MandatoryFields = keyof Parameters;
+}
