920c9253e0335e4368058f7278dfa77aab9c0934
CauaHenrique19 <cauah6002@gmail.com>
2022-11-07 22:43:45 -0300

    Implementing transaction manager in usecase

--- src/data/usecases/channel/db-create-channel.ts
+++ src/data/usecases/channel/db-create-channel.ts
@@ -1,11 +1,13 @@
 import { ChannelStatusEnum, UserChannelStatusEnum } from 'src/data/enums';
 import { CreateChannelRepository, CreateUserChannelRepository } from 'src/data/protocols/db';
+import { TransactionManager } from 'src/data/protocols/transaction-manager';
 import { CreateChannelUseCase } from 'src/domain/usecases';
 
 export class DbCreateChannel implements CreateChannelUseCase {
   constructor(
     private readonly createChannelRepository: CreateChannelRepository,
     private readonly createUserChannelRepository: CreateUserChannelRepository,
+    private readonly transactionManager: TransactionManager,
   ) {}
 
   async create(parameters: CreateChannelUseCase.Parameters): Promise<CreateChannelUseCase.Result> {
@@ -16,16 +18,20 @@ export class DbCreateChannel implements CreateChannelUseCase {
       createdAt: new Date(),
     };
 
-    const createdChannel = await this.createChannelRepository.create(channel);
+    const createdChannel = await this.transactionManager.handleTransaction(async (transaction) => {
+      const createdChannel = await this.createChannelRepository.create(channel, transaction);
 
-    const usersInChannel: CreateUserChannelRepository.Parameters = parameters.users.map((userId) => ({
-      userId,
-      channelId: createdChannel.id,
-      createdAt: new Date(),
-      status: UserChannelStatusEnum.ACTIVE,
-    }));
+      const usersInChannel: CreateUserChannelRepository.Parameters = parameters.users.map((userId) => ({
+        userId,
+        channelId: createdChannel.id,
+        createdAt: new Date(),
+        status: UserChannelStatusEnum.ACTIVE,
+      }));
 
-    await this.createUserChannelRepository.create(usersInChannel);
+      await this.createUserChannelRepository.create(usersInChannel, transaction);
+
+      return createdChannel;
+    });
 
     return createdChannel;
   }
--- src/main/factories/usecases/channel/create-channel.factory.ts
+++ src/main/factories/usecases/channel/create-channel.factory.ts
@@ -4,11 +4,16 @@ import { ChannelPrismaRepository } from 'src/infra/prisma/repositories/channel';
 import { DbCreateChannel } from 'src/data/usecases/channel';
 import { CreateChannelUseCase } from 'src/domain/usecases';
 import { UserChannelPrismaRepository } from 'src/infra/prisma/repositories/user-channel';
+import { PrismaTransactionManager } from 'src/infra/prisma/transaction-manager';
 
 export const createChannelFactory: Provider = {
   provide: CREATE_CHANNEL_FACTORY,
-  useFactory(chanelRepository: ChannelPrismaRepository, userChannelRepository: UserChannelPrismaRepository): CreateChannelUseCase {
-    return new DbCreateChannel(chanelRepository, userChannelRepository);
+  useFactory(
+    chanelRepository: ChannelPrismaRepository,
+    userChannelRepository: UserChannelPrismaRepository,
+    transactionManager: PrismaTransactionManager,
+  ): CreateChannelUseCase {
+    return new DbCreateChannel(chanelRepository, userChannelRepository, transactionManager);
   },
-  inject: [ChannelPrismaRepository, UserChannelPrismaRepository],
+  inject: [ChannelPrismaRepository, UserChannelPrismaRepository, PrismaTransactionManager],
 };
