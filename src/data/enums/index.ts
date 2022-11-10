23e88b45072d20d353ab2054b5e2cf97ca9da462
CauaHenrique19 <cauah6002@gmail.com>
2022-11-10 00:05:37 -0300

    Creating enum to specify events of websocket

--- src/data/enums/index.ts
+++ src/data/enums/index.ts
@@ -2,3 +2,4 @@ export * from './client-status-enum';
 export * from './user-status-enum';
 export * from './channel-status-enum';
 export * from './user-channel-status-enum';
+export * from './websocket-events-enum';
---
+++ src/data/enums/websocket-events-enum.ts
@@ -0,0 +1,7 @@
+export enum WebsocketEventsEnum {
+  GET_QR_CODE = 'get_qr_code',
+  NEW_QR_CODE = 'new_qr_code',
+  CONNECTED = 'connected',
+  ALLOW_RECEIVE_MESSAGES = 'allow_receive_messages',
+  RESULT_ALLOW_RECEIVE_MESSAGES = 'result_allow_receive_messages',
+}
