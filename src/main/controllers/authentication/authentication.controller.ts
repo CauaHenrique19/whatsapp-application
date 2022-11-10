9d1098e521ee8b1cb716dd023d403dc056e21961
CauaHenrique19 <cauah6002@gmail.com>
2022-11-10 00:27:30 -0300

    Adding solicitation to start connection in whatsapp in authentication controller of nest

--- src/main/controllers/authentication/authentication.controller.ts
+++ src/main/controllers/authentication/authentication.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Post, Response } from '@nestjs/common';
+import { Body, Controller, Post, Response, Headers } from '@nestjs/common';
 import { controllerAdapter } from 'src/main/adapters';
 import { BuildLoginControllerFactory } from 'src/main/factories/controllers';
+import { BuildConnectToWhatsappGatewayFactory } from 'src/main/factories/gateways/authentication';
 
 @Controller('authentication')
 export class AuthenticationController {
-  constructor(private readonly buildLoginControllerFactory: BuildLoginControllerFactory) {}
+  constructor(
+    private readonly buildLoginControllerFactory: BuildLoginControllerFactory,
+    private readonly buildConnectToWhatsappGatewayFactory: BuildConnectToWhatsappGatewayFactory,
+  ) {}
 
   @Post('/login')
   async createClient(@Body() body, @Response() response) {
     const result = await controllerAdapter(this.buildLoginControllerFactory.build(), { data: body });
+    return response.status(result.statusCode).json(result);
+  }
+
+  @Post('/connect-to-whatsapp')
+  async connectToWhatsapp(@Body() data, @Headers('authorization') token, @Response() response) {
+    const result = await controllerAdapter(this.buildConnectToWhatsappGatewayFactory.build(), { data, token });
     return response.status(result.statusCode).json(result);
   }
 }
--- src/main/controllers/authentication/authentication.module.ts
+++ src/main/controllers/authentication/authentication.module.ts
@@ -2,10 +2,11 @@ import { AuthenticationController } from './authentication.controller';
 import { Module } from '@nestjs/common';
 import { BuildLoginControllerFactory } from 'src/main/factories/controllers';
 import { FactoryModule } from 'src/main/factories/usecases/factory.module';
+import { BuildConnectToWhatsappGatewayFactory } from 'src/main/factories/gateways/authentication';
 
 @Module({
   imports: [FactoryModule],
   controllers: [AuthenticationController],
-  providers: [BuildLoginControllerFactory],
+  providers: [BuildLoginControllerFactory, BuildConnectToWhatsappGatewayFactory],
 })
 export class AuthenticationModule {}
