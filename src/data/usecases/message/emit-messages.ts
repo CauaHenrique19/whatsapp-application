c0e59bc348d605bfe0a4734cc14b9cf517a708e6
CauaHenrique19 <cauah6002@gmail.com>
2022-11-12 13:37:50 -0300

    Implementing verification if is first conversation with participant

--- src/data/usecases/message/emit-messages.ts
+++ src/data/usecases/message/emit-messages.ts
@@ -1,18 +1,50 @@
-import { WebsocketEventsEnum } from 'src/data/enums';
+import { ChatStatusEnum, WebsocketEventsEnum } from 'src/data/enums';
+import { CreateChatRepository, GetChatByNumberParticipantRepository, UpdateChatRepository } from 'src/data/protocols/db';
 import { MultitonInterface } from 'src/data/protocols/multiton';
 import { WebsocketInterface } from 'src/data/protocols/websocket';
 import { WhatsappClientInterface } from 'src/data/protocols/whatsapp';
-import { WhatsappMessageModel } from 'src/domain/models';
+import { ChatModel, WhatsappMessageModel } from 'src/domain/models';
 import { EmitMessagesUseCase } from 'src/domain/usecases';
 
 export class EmitMessages implements EmitMessagesUseCase {
-  constructor(private readonly multiton: MultitonInterface<WhatsappClientInterface>, private readonly websocketAdapter: WebsocketInterface) {}
+  constructor(
+    private readonly multiton: MultitonInterface<WhatsappClientInterface>,
+    private readonly websocketAdapter: WebsocketInterface,
+    private readonly getChatByNumberParticipantRepository: GetChatByNumberParticipantRepository,
+    private readonly createChatRepository: CreateChatRepository,
+    private readonly updateChatRepository: UpdateChatRepository,
+  ) {}
 
   async emit(parameters: EmitMessagesUseCase.Parameters): Promise<EmitMessagesUseCase.Result> {
     const { clientId } = parameters;
 
     const client = await this.multiton.getInstance(clientId);
-    client.instance.onMessage((message) => {
+    client.instance.onMessage(async (message) => {
+      const numberParticipant = message.from;
+      const chat = await this.getChatByNumberParticipantRepository.getByNumberParticipant({ number: numberParticipant });
+
+      let finalChat: ChatModel = chat;
+
+      if (chat) {
+        if (chat.status === ChatStatusEnum.FINISHED) {
+          chat.userId = null;
+          chat.status = ChatStatusEnum.WAITING_USER;
+
+          finalChat = await this.updateChatRepository.update(chat);
+          //SEND AUTOMATIC MESSAGE
+        }
+      } else {
+        const chat: ChatModel = {
+          numberParticipant: numberParticipant,
+          status: ChatStatusEnum.WAITING_USER,
+        };
+
+        finalChat = await this.createChatRepository.create(chat);
+        //SEND AUTOMATIC MESSAGE
+      }
+
+      //SAVE RECEIVED MESSAGE
+      console.log(finalChat);
       this.websocketAdapter.emitEvent<WhatsappMessageModel>(WebsocketEventsEnum.NEW_MESSAGE, message);
     });
 
--- src/main/factories/usecases/message/emit-messages.factory.ts
+++ src/main/factories/usecases/message/emit-messages.factory.ts
@@ -4,13 +4,18 @@ import { WhatsappClientInterface } from 'src/data/protocols/whatsapp';
 import { EmitMessages } from 'src/data/usecases/message';
 import { EmitMessagesUseCase } from 'src/domain/usecases';
 import { Multiton } from 'src/infra/multiton';
+import { ChatPrismaRepository } from 'src/infra/prisma/repositories/chat';
 import { SocketIoAdapter } from 'src/infra/websocket/socket-io';
 import { EMIT_MESSAGES_FACTORY } from '../../providers';
 
 export const emitMessagesFactory: Provider = {
   provide: EMIT_MESSAGES_FACTORY,
-  useFactory: (multitonProvider: MultitonInterface<WhatsappClientInterface>, socketIoAdapter: SocketIoAdapter): EmitMessagesUseCase => {
-    return new EmitMessages(multitonProvider, socketIoAdapter);
+  useFactory: (
+    multitonProvider: MultitonInterface<WhatsappClientInterface>,
+    socketIoAdapter: SocketIoAdapter,
+    chatRepository: ChatPrismaRepository,
+  ): EmitMessagesUseCase => {
+    return new EmitMessages(multitonProvider, socketIoAdapter, chatRepository, chatRepository, chatRepository);
   },
-  inject: [Multiton, SocketIoAdapter],
+  inject: [Multiton, SocketIoAdapter, ChatPrismaRepository],
 };
