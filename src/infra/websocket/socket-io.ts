100a4d26151579660ce2f1421b6d8c134cf60cfa
CauaHenrique19 <cauah6002@gmail.com>
2022-11-13 19:16:56 -0300

    Implementing flow of connection and emit events to rooms

--- src/infra/websocket/socket-io.ts
+++ src/infra/websocket/socket-io.ts
@@ -1,29 +1,54 @@
 import { Server, Socket } from 'socket.io';
 import { WebsocketEventsEnum } from 'src/data/enums';
+import { Decrypter } from 'src/data/protocols/cryptography';
 import { WebsocketInterface } from 'src/data/protocols/websocket';
+import { JwtAdapter } from '../cryptography/jwt-adapter';
 
 export class SocketIoAdapter implements WebsocketInterface {
-  socketServer: Server;
+  private socketServer: Server;
+  private decrypter: Decrypter = new JwtAdapter(process.env.SECRET);
 
   constructor() {
     const sv = new Server();
     this.socketServer = sv;
 
     this.socketServer.listen(parseInt(process.env.WEBSOCKET_PORT));
-    this.socketServer.on('connection', this.connection);
+    this.socketServer.on('connection', this.connection.bind(this));
   }
 
   emitEvent<T>(event: WebsocketEventsEnum, data: T): void {
     this.socketServer.emit(event, data);
   }
 
+  emitEventToRooms<T>(rooms: string[], event: WebsocketEventsEnum, data: T) {
+    const hasRooms = !!rooms.length;
+
+    if (hasRooms) {
+      this.socketServer.to(rooms).emit(event, data);
+    }
+  }
+
   listenEvent<T>(event: WebsocketEventsEnum, listener: (arg: T) => void): void {
     this.socketServer.on(event, listener);
   }
 
-  private connection(socket: Socket) {
+  private async connection(socket: Socket) {
     const token = socket.request.headers.authorization;
-    console.log('connection');
+
+    if (!token) {
+      socket.disconnect();
+      return;
+    }
+
+    const infoToken = await this.decrypter.decrypt(token);
+
+    if (!infoToken) {
+      socket.disconnect();
+      return;
+    }
+
+    socket.join(infoToken.email);
+    socket.on('disconnect', this.disconnect);
   }
 
   private disconnect() {
