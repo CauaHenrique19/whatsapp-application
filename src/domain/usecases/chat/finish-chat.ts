9ca1a136a493530e9b1c6c7ea143b36e172bdaab
CauaHenrique19 <cauah6002@gmail.com>
2022-11-13 12:35:22 -0300

    Creating usecase to finish chat

---
+++ src/domain/usecases/chat/finish-chat.ts
@@ -0,0 +1,12 @@
+import { ChatModel } from 'src/domain/models';
+
+export interface FinishChatUseCase {
+  finish(parameters: FinishChatUseCase.Parameters): Promise<FinishChatUseCase.Result>;
+}
+
+export namespace FinishChatUseCase {
+  export type Parameters = {
+    chatId: number;
+  };
+  export type Result = Omit<ChatModel, 'user' | 'channel'>;
+}
--- src/domain/usecases/chat/index.ts
+++ src/domain/usecases/chat/index.ts
@@ -2,3 +2,4 @@ export * from './create-chat';
 export * from './get-chat-by-number-participant';
 export * from './update-chat';
 export * from './attach-chat-to-user';
+export * from './finish-chat';
