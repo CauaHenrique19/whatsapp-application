a6598b31e3d6e159b89787213fe64e6fece70007
CauaHenrique19 <cauah6002@gmail.com>
2022-11-12 21:09:01 -0300

    Implementing get channel by client in repository

--- src/infra/prisma/repositories/channel/channel.prisma-repository.ts
+++ src/infra/prisma/repositories/channel/channel.prisma-repository.ts
@@ -1,7 +1,8 @@
 import { Prisma, PrismaClient } from '@prisma/client';
-import { CreateChannelRepository } from 'src/data/protocols/db';
+import { ChannelStatusEnum, UserStatusEnum } from 'src/data/enums';
+import { CreateChannelRepository, GetChannelsByClientIdRepository } from 'src/data/protocols/db';
 
-export class ChannelPrismaRepository implements CreateChannelRepository {
+export class ChannelPrismaRepository implements CreateChannelRepository, GetChannelsByClientIdRepository {
   private channelRepository: Prisma.ChannelDelegate<Prisma.RejectOnNotFound | Prisma.RejectPerOperation>;
 
   constructor() {
@@ -9,8 +10,37 @@ export class ChannelPrismaRepository implements CreateChannelRepository {
     this.channelRepository = prisma.channel;
   }
 
-  async create(parameters: CreateChannelRepository.Parameters, transaction?: Prisma.TransactionClient): Promise<CreateChannelRepository.Result> {
+  async create(
+    parameters: CreateChannelRepository.Parameters,
+    transaction?: Prisma.TransactionClient,
+  ): Promise<CreateChannelRepository.Result> {
     const client = transaction ? transaction.channel : this.channelRepository;
     return await client.create({ data: parameters });
+  }
+
+  async getByClientId(parameters: GetChannelsByClientIdRepository.Parameters): Promise<GetChannelsByClientIdRepository.Result> {
+    const channels = await this.channelRepository.findMany({
+      where: {
+        clientId: parameters.clientId,
+        status: ChannelStatusEnum.ACTIVE,
+      },
+      include: {
+        users: {
+          select: {
+            id: true,
+            email: true,
+            name: true,
+            lastName: true,
+            status: true,
+            createdAt: true,
+          },
+          where: {
+            status: UserStatusEnum.ACTIVE,
+          },
+        },
+      },
+    });
+
+    return channels;
   }
 }
