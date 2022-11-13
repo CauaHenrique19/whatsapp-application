7d87a3fb7ceec84095f9c573a31c2e4f44e1891a
CauaHenrique19 <cauah6002@gmail.com>
2022-11-12 23:21:17 -0300

    Adding function to send message

--- src/data/protocols/whatsapp/whatsapp-client.ts
+++ src/data/protocols/whatsapp/whatsapp-client.ts
@@ -1,7 +1,8 @@
-import { WhatsappChatModel, WhatsappMessageModel } from 'src/domain/models';
+import { WhatsappChatModel, WhatsappList, WhatsappMessageModel } from 'src/domain/models';
 
 //generic contract for implementation
 export interface WhatsappClientInterface {
   onMessage(cb: (message: WhatsappMessageModel) => void);
   getUnreadChats(): Promise<WhatsappChatModel[]>;
+  sendMessage(number: string, content: WhatsappList | string);
 }
