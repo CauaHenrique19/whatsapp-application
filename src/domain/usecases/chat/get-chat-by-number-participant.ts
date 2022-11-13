93897cc961e4f104e1d6d6b8127b2c277bdafa40
CauaHenrique19 <cauah6002@gmail.com>
2022-11-13 19:18:20 -0300

    Adding user

--- src/domain/usecases/chat/get-chat-by-number-participant.ts
+++ src/domain/usecases/chat/get-chat-by-number-participant.ts
@@ -1,4 +1,5 @@
 import { ChatModel } from 'src/domain/models';
+import { UserModel } from 'src/domain/models/user';
 
 export interface GetChatByNumberParticipantUseCase {
   getByNumberParticipant(parameters: GetChatByNumberParticipantUseCase.Parameters): Promise<GetChatByNumberParticipantUseCase.Result>;
@@ -8,5 +9,5 @@ export namespace GetChatByNumberParticipantUseCase {
   export type Parameters = {
     number: string;
   };
-  export type Result = Omit<ChatModel, 'user'>;
+  export type Result = Omit<ChatModel, 'user'> & { user: Omit<UserModel, 'client' | 'password'> };
 }
