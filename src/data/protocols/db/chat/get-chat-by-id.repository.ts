524ac975f70f4f482711294116bedb8ccf624421
CauaHenrique19 <cauah6002@gmail.com>
2022-11-13 14:16:31 -0300

    Creating protocol to get chat by id

---
+++ src/data/protocols/db/chat/get-chat-by-id.repository.ts
@@ -0,0 +1,13 @@
+import { ChatModel } from 'src/domain/models';
+
+export interface GetChatByIdRepository {
+  getById(parameters: GetChatByIdRepository.Parameters): Promise<GetChatByIdRepository.Result>;
+}
+
+export namespace GetChatByIdRepository {
+  export type Parameters = {
+    id: number;
+  };
+
+  export type Result = Omit<ChatModel, 'user' | 'channel'>;
+}
--- src/data/protocols/db/chat/index.ts
+++ src/data/protocols/db/chat/index.ts
@@ -2,3 +2,4 @@ export * from './create-chat.repository';
 export * from './get-chat-by-number-participant.repository';
 export * from './update-chat.repository';
 export * from './create-chat-log.repository';
+export * from './get-chat-by-id.repository';
