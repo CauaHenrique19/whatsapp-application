53d2b6a8efc5a1443c8d65c017c53f1e6786d0ca
CauaHenrique19 <cauah6002@gmail.com>
2022-11-13 19:16:20 -0300

    Adding function to emit event to rooms

--- src/data/protocols/websocket/websocket-interface.ts
+++ src/data/protocols/websocket/websocket-interface.ts
@@ -2,5 +2,6 @@ import { WebsocketEventsEnum } from 'src/data/enums';
 
 export interface WebsocketInterface {
   emitEvent<T>(event: WebsocketEventsEnum, data: T): void;
+  emitEventToRooms<T>(rooms: string[], event: WebsocketEventsEnum, data: T);
   listenEvent<T>(event: WebsocketEventsEnum, listener: (arg: T) => void): void;
 }
