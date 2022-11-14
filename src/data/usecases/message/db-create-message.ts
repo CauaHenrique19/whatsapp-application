6f83d3e59603d14ccf5eaa89ef1f72c9a4f22ca6
CauaHenrique19 <cauah6002@gmail.com>
2022-11-14 00:19:50 -0300

    Sending message to whatsapp

--- src/data/usecases/message/db-create-message.ts
+++ src/data/usecases/message/db-create-message.ts
@@ -1,16 +1,33 @@
-import { CreateMessageRepository } from 'src/data/protocols/db';
+import { CreateMessageRepository, GetChatByIdRepository } from 'src/data/protocols/db';
+import { MultitonInterface } from 'src/data/protocols/multiton';
+import { WhatsappClientInterface } from 'src/data/protocols/whatsapp';
 import { CreateMessageUseCase } from 'src/domain/usecases';
 
 export class DbCreateMessage implements CreateMessageUseCase {
-  constructor(private readonly createMessageRepository: CreateMessageRepository) {}
+  constructor(
+    private readonly createMessageRepository: CreateMessageRepository,
+    private readonly getChatByIdRepository: GetChatByIdRepository,
+    private readonly multiton: MultitonInterface<WhatsappClientInterface>,
+  ) {}
 
   async create(parameters: CreateMessageUseCase.Parameters): Promise<CreateMessageUseCase.Result> {
-    const message: CreateMessageRepository.Parameters = {
-      ...parameters,
+    const messageToCreate: CreateMessageRepository.Parameters = {
+      chatId: parameters.chatId,
+      content: parameters.content,
+      userId: parameters.user.id,
       fromParticipant: true,
       createdAt: new Date(),
     };
 
-    return await this.createMessageRepository.create(message);
+    const message = await this.createMessageRepository.create(messageToCreate);
+    const chat = await this.getChatByIdRepository.getById({ id: parameters.chatId });
+
+    const { instance: client } = await this.multiton.getInstance(parameters.user.clientId);
+
+    const templateMessage = `*${chat.channel.name} - ${parameters.user.name} ${parameters.user.lastName}*
+      \n${parameters.content}`;
+    await client.sendMessage(chat.numberParticipant, templateMessage);
+
+    return message;
   }
 }
--- src/main/factories/usecases/message/create-message.factory.ts
+++ src/main/factories/usecases/message/create-message.factory.ts
@@ -1,13 +1,21 @@
 import { Provider } from '@nestjs/common';
+import { MultitonInterface } from 'src/data/protocols/multiton';
+import { WhatsappClientInterface } from 'src/data/protocols/whatsapp';
 import { DbCreateMessage } from 'src/data/usecases/message';
 import { CreateMessageUseCase } from 'src/domain/usecases';
+import { Multiton } from 'src/infra/multiton';
+import { ChatPrismaRepository } from 'src/infra/prisma/repositories/chat';
 import { MessagePrismaRepository } from 'src/infra/prisma/repositories/message';
 import { CREATE_MESSAGE_FACTORY } from '../../providers';
 
 export const createMessageFactory: Provider = {
   provide: CREATE_MESSAGE_FACTORY,
-  useFactory: (messageRepository: MessagePrismaRepository): CreateMessageUseCase => {
-    return new DbCreateMessage(messageRepository);
+  useFactory: (
+    messageRepository: MessagePrismaRepository,
+    chatRepository: ChatPrismaRepository,
+    multiton: MultitonInterface<WhatsappClientInterface>,
+  ): CreateMessageUseCase => {
+    return new DbCreateMessage(messageRepository, chatRepository, multiton);
   },
-  inject: [MessagePrismaRepository],
+  inject: [MessagePrismaRepository, ChatPrismaRepository, Multiton],
 };
