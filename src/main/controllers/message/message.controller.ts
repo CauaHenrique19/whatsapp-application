42ce6f297bb545c2528f58b3c337d3d35542aa72
CauaHenrique19 <cauah6002@gmail.com>
2022-11-13 23:05:48 -0300

    Creating endpoint to receive requisitons of create message

--- src/main/controllers/message/message.controller.ts
+++ src/main/controllers/message/message.controller.ts
@@ -1,14 +1,23 @@
 import { Body, Controller, Post, Response, Headers } from '@nestjs/common';
 import { controllerAdapter } from 'src/main/adapters';
-import { BuildEmitMessageControllerFactory } from 'src/main/factories/controllers/message';
+import { BuildCreateMessageControllerFactory, BuildEmitMessageControllerFactory } from 'src/main/factories/controllers/message';
 
 @Controller('messages')
 export class MessageController {
-  constructor(private readonly buildEmitMessageControllerFactory: BuildEmitMessageControllerFactory) {}
+  constructor(
+    private readonly buildEmitMessageControllerFactory: BuildEmitMessageControllerFactory,
+    private readonly buildCreateMessageControllerFactory: BuildCreateMessageControllerFactory,
+  ) {}
 
   @Post('allow-receive')
   async allowReceiveMessages(@Body() data, @Headers('authorization') token, @Response() response) {
     const result = await controllerAdapter(this.buildEmitMessageControllerFactory.build(), { data, token });
+    return response.status(result.statusCode).json(result);
+  }
+
+  @Post('send')
+  async sendMessage(@Body() data, @Headers('authorization') token, @Response() response) {
+    const result = await controllerAdapter(this.buildCreateMessageControllerFactory.build(), { data, token });
     return response.status(result.statusCode).json(result);
   }
 }
--- src/main/controllers/message/message.module.ts
+++ src/main/controllers/message/message.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { BuildEmitMessageControllerFactory } from 'src/main/factories/controllers';
+import { BuildCreateMessageControllerFactory, BuildEmitMessageControllerFactory } from 'src/main/factories/controllers';
 import { FactoryModule } from 'src/main/factories/usecases/factory.module';
 import { MessageController } from './message.controller';
 
 @Module({
   imports: [FactoryModule],
   controllers: [MessageController],
-  providers: [BuildEmitMessageControllerFactory],
+  providers: [BuildEmitMessageControllerFactory, BuildCreateMessageControllerFactory],
 })
 export class MessageModule {}
