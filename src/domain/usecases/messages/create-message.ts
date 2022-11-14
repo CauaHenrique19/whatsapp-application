061a27b8df973e4a44fc7a0e67a9d33e8439a4c1
CauaHenrique19 <cauah6002@gmail.com>
2022-11-13 23:00:03 -0300

    Creating usecase to create message

---
+++ src/domain/usecases/messages/create-message.ts
@@ -0,0 +1,10 @@
+import { MessageModel } from 'src/domain/models';
+
+export interface CreateMessageUseCase {
+  create(parameters: CreateMessageUseCase.Parameters): Promise<CreateMessageUseCase.Result>;
+}
+
+export namespace CreateMessageUseCase {
+  export type Parameters = Omit<MessageModel, 'id' | 'user' | 'chat' | 'createdAt' | 'fromParticipant'>;
+  export type Result = Omit<MessageModel, 'user' | 'chat'>;
+}
