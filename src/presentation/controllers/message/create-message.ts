ba789145b718f8685d3ae4c1ce4f27c4db5be2c5
CauaHenrique19 <cauah6002@gmail.com>
2022-11-13 23:04:47 -0300

    Implementing controller to make validation and make call to create message usecase

---
+++ src/presentation/controllers/message/create-message.ts
@@ -0,0 +1,31 @@
+import { MessageModel } from 'src/domain/models';
+import { CreateMessageUseCase } from 'src/domain/usecases';
+import { MissingParamError } from 'src/presentation/errors';
+import { badRequest, ok, serverError } from 'src/presentation/helpers';
+import { Controller, ControllerData, HttpResponse } from 'src/presentation/protocols';
+
+export class CreateMessageController implements Controller {
+  constructor(private readonly createMessageUseCase: CreateMessageUseCase) {}
+
+  async handle(parameters: ControllerData<CreateMessageController.Parameters>): Promise<HttpResponse> {
+    try {
+      const mandatoryFields: CreateMessageController.MandatoryFields[] = ['chatId', 'content'];
+
+      for (const field of mandatoryFields) {
+        if (parameters.data[field] === null || parameters.data[field] === undefined) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+
+      const result = await this.createMessageUseCase.create({ ...parameters.data, userId: parameters.user.id });
+      return ok(result);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
+
+export namespace CreateMessageController {
+  export type Parameters = Pick<MessageModel, 'chatId' | 'content'>;
+  export type MandatoryFields = keyof Parameters;
+}
--- src/presentation/controllers/message/index.ts
+++ src/presentation/controllers/message/index.ts
@@ -1,1 +1,2 @@
 export * from './emit-messages';
+export * from './create-message';
