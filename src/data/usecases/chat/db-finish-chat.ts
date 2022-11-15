b22964f6c8eaa485216e3c4772a0373e5a61b1a3
CauaHenrique19 <cauah6002@gmail.com>
2022-11-15 14:09:59 -0300

    Sending automatic message of avaliation

--- src/data/usecases/chat/db-finish-chat.ts
+++ src/data/usecases/chat/db-finish-chat.ts
@@ -2,6 +2,7 @@ import { ChatLogTypeActionEnum, ChatStatusEnum } from 'src/data/enums';
 import { CreateChatLogRepository, UpdateChatRepository, GetChatByIdRepository } from 'src/data/protocols/db';
 import { MultitonInterface } from 'src/data/protocols/multiton';
 import { WhatsappClientInterface } from 'src/data/protocols/whatsapp';
+import { WhatsappList, WhatsappSection } from 'src/domain/models';
 import { FinishChatUseCase } from 'src/domain/usecases';
 
 export class DbFinishChat implements FinishChatUseCase {
@@ -40,6 +41,61 @@ export class DbFinishChat implements FinishChatUseCase {
       `Seu atendimento foi finalizado, a partir desse momento qualquer mensagem enviada iniciará um novo atendimento. Tenha um ótimo dia!`,
     );
 
+    await this.sendSelectNoteOfChatAutomaticMessage(loadedChat.numberParticipant, client);
+
     return await this.updateChatRepository.update(chat);
+  }
+
+  async sendSelectNoteOfChatAutomaticMessage(number: string, client: WhatsappClientInterface) {
+    const rowsSections = [
+      {
+        id: 'avaliation_1',
+        title: '⭐',
+        description: 'Muito Ruim',
+      },
+      {
+        id: 'avaliation_2',
+        title: '⭐⭐',
+        description: 'Ruim',
+      },
+      {
+        id: 'avaliation_3',
+        title: '⭐⭐⭐',
+        description: 'Regular',
+      },
+      {
+        id: 'avaliation_4',
+        title: '⭐⭐⭐⭐',
+        description: 'Muito Bom',
+      },
+      {
+        id: 'avaliation_5',
+        title: '⭐⭐⭐⭐⭐',
+        description: 'Ótimo',
+      },
+    ];
+
+    let sections: WhatsappSection[] = [
+      {
+        title: 'Selecione a opção desejada.',
+        rows: rowsSections,
+      },
+    ];
+
+    const text = `👋 Olá!! 
+    \nAgora que seu atendimento foi finalizado queremos saber a sua opinião sobre o atendimento que você recebeu.
+    \nSelecione a opção de acordo com a sua opinião, por favor não deixe de avaliar o nosso atendimento, essa informação é muito valiosa para nós continuarmos prestando um ótimo serviço ao cliente e melhorando conforme as avaliações negativas.
+    \nLembrando que sua resposta só será computada se você selecionar uma das opções na lista abaixo.
+    \n🤖 _Esta é uma mensagem automática_`;
+
+    const list: WhatsappList = {
+      body: text,
+      buttonText: 'Avaliar',
+      sections,
+      title: 'Avaliação',
+      footer: 'Esta é uma mensagem automática.',
+    };
+
+    await client.sendMessage(number, list);
   }
 }
