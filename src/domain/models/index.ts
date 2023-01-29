8bfe09c597413fefe3b02608ea86418766353198
Cauã Henrique <caua.henrique@squadra.com.br>
2023-01-28 21:13:37 -0300

    creating pre defined message model

--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -5,3 +5,4 @@ export * from './client';
 export * from './channel';
 export * from './user-channel';
 export * from './avaliation';
+export * from './pre-defined-message';
---
+++ src/domain/models/pre-defined-message/pre-defined-message.model.ts
@@ -0,0 +1,7 @@
+export interface PreDefinedMessageModel {
+  id: number;
+  content: string;
+  userId: number;
+  status: number;
+  createdAt: Date;
+}
