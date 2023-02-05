c502e73ab0f220e432eac8d3a0023d2c9179d849
CauaHenrique19 <cauah6002@gmail.com>
2023-02-05 17:36:21 -0300

    creating endpoint to get pre defined messages by user id

--- src/main/controllers/pre-defined-message/pre-defined-message.controller.ts
+++ src/main/controllers/pre-defined-message/pre-defined-message.controller.ts
@@ -1,14 +1,26 @@
-import { Body, Controller, Response, Headers, Post } from '@nestjs/common';
+import { Body, Controller, Response, Headers, Post, Get, Param } from '@nestjs/common';
 import { controllerAdapter } from 'src/main/adapters';
-import { BuildCreatePreDefinedMessageControllerFactory } from 'src/main/factories/controllers/pre-defined-message';
+import {
+  BuildCreatePreDefinedMessageControllerFactory,
+  BuildGetPreDefinedMessageByUserIdFactory,
+} from 'src/main/factories/controllers/pre-defined-message';
 
 @Controller('pre-defined-messages')
 export class PreDefinedMessageController {
-  constructor(private readonly buildCreatePreDefinedMessageControllerFactory: BuildCreatePreDefinedMessageControllerFactory) {}
+  constructor(
+    private readonly buildCreatePreDefinedMessageControllerFactory: BuildCreatePreDefinedMessageControllerFactory,
+    private readonly buildGetPreDefinedMessageByUserIdFactory: BuildGetPreDefinedMessageByUserIdFactory,
+  ) {}
 
   @Post()
   async createPreDefinedMessage(@Body() data, @Headers('authorization') token, @Response() response) {
     const result = await controllerAdapter(this.buildCreatePreDefinedMessageControllerFactory.build(), { data, token });
+    return response.status(result.statusCode).json(result);
+  }
+
+  @Get()
+  async getPreDefinedMessageByUserId(@Headers('authorization') token, @Response() response) {
+    const result = await controllerAdapter(this.buildGetPreDefinedMessageByUserIdFactory.build(), { data: null, token });
     return response.status(result.statusCode).json(result);
   }
 }
--- src/main/controllers/pre-defined-message/pre-defined-message.module.ts
+++ src/main/controllers/pre-defined-message/pre-defined-message.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
-import { BuildCreatePreDefinedMessageControllerFactory } from 'src/main/factories/controllers/pre-defined-message';
+import {
+  BuildCreatePreDefinedMessageControllerFactory,
+  BuildGetPreDefinedMessageByUserIdFactory,
+} from 'src/main/factories/controllers/pre-defined-message';
 import { FactoryModule } from 'src/main/factories/usecases/factory.module';
 import { PreDefinedMessageController } from './pre-defined-message.controller';
 
 @Module({
   imports: [FactoryModule],
   controllers: [PreDefinedMessageController],
-  providers: [BuildCreatePreDefinedMessageControllerFactory],
+  providers: [BuildCreatePreDefinedMessageControllerFactory, BuildGetPreDefinedMessageByUserIdFactory],
 })
 export class PreDefinedMessageModule {}
