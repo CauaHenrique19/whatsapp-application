a42a3468b620320531c4e6a0b8f781f4e878ac87
CauaHenrique19 <cauah6002@gmail.com>
2023-02-26 00:21:40 -0300

    implementing interface of find and update message

--- src/infra/prisma/repositories/message/message.prisma-repository.ts
+++ src/infra/prisma/repositories/message/message.prisma-repository.ts
@@ -1,7 +1,7 @@
 import { Prisma, PrismaClient } from '@prisma/client';
-import { CreateMessageRepository } from 'src/data/protocols/db';
+import { CreateMessageRepository, UpdateMessageRepository, FindMessageRepository } from 'src/data/protocols/db';
 
-export class MessagePrismaRepository implements CreateMessageRepository {
+export class MessagePrismaRepository implements CreateMessageRepository, UpdateMessageRepository, FindMessageRepository {
   private messageRepository: Prisma.MessageDelegate<Prisma.RejectOnNotFound | Prisma.RejectPerOperation>;
 
   constructor() {
@@ -13,5 +13,41 @@ export class MessagePrismaRepository implements CreateMessageRepository {
     return await this.messageRepository.create({
       data: parameters,
     });
+  }
+
+  async update(parameters: UpdateMessageRepository.Parameters): Promise<UpdateMessageRepository.Result> {
+    const updated = await this.messageRepository.update({
+      data: parameters,
+      where: {
+        id: parameters.id,
+        whatsappMessageId: parameters.whatsappMessageId,
+      },
+    });
+
+    return updated;
+  }
+
+  async findMessage(parameters: FindMessageRepository.Parameters): Promise<FindMessageRepository.Result> {
+    const message = (await this.messageRepository.findFirst({
+      where: {
+        OR: [
+          {
+            id: parameters.id,
+          },
+          {
+            whatsappMessageId: parameters.whatsappMessageId,
+          },
+        ],
+      },
+      include: {
+        chat: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    })) as FindMessageRepository.Result;
+
+    return message;
   }
 }
